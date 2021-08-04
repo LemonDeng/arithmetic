8598ade403ef1d9700bf8fed9278f7100b3ef014
DZL <593529172@qq.com>
2021-08-04 14:16:22 +0800

    链表LeetCode203移除链表元素

---
+++ linkedList/src/linkedListLeetCode203/LinkedListLeetCode203.java
@@ -0,0 +1,56 @@
+package linkedListLeetCode203;
+
+import java.util.LinkedList;
+
+/**
+ * @ClassName:LinkedListLeetCode203
+ * @Description: 给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+ * * public class ListNode {
+ * *     int val;
+ * *     ListNode next;
+ * *     ListNode() {}
+ * *     ListNode(int val) { this.val = val; }
+ * *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * * }
+ * @Author: Deng Zhi Li
+ * @Date: 2021/8/4 13:43
+ */
+public class LinkedListLeetCode203 {
+   
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public  ListNode  removeElements(ListNode head, int val) {
+        ListNode temp = new ListNode(0);
+        temp.next = head;
+        ListNode pre = temp;
+        while (head != null && head.next != null)
+        {
+            if (head.val == val)
+            {
+                pre.next = head.next;
+                head = head.next;
+            }else
+            {
+                pre = head;
+                head = head.next;
+            }
+        }
+        return temp.next;
+
+    }
+}
