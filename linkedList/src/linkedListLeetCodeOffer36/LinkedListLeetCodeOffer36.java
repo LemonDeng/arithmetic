789eadf6a9cc1f18bb5daaad2703bb510a5756b3
DZL <593529172@qq.com>
2021-08-11 21:53:31 +0800

    剑指 Offer 36. 二叉搜索树与双向链表

---
+++ linkedList/src/linkedListLeetCodeOffer36/LinkedListLeetCodeOffer36.java
@@ -0,0 +1,56 @@
+package linkedListLeetCodeOffer36;
+
+/**
+ * @ClassName:LinkedListLeetCodeOffer36
+ * @Description: 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的循环双向链表。要求不能创建任何新的节点，只能调整树中节点指针的指向。
+ * @Author: Deng Zhi Li
+ * @Date: 2021/8/10 15:12
+ */
+public class LinkedListLeetCodeOffer36 {
+    class  Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val,Node _left,Node _right) {
+            val = _val;
+            left = _left;
+            right = _right;
+        }
+    };
+
+    // 1. 中序，递归，来自解题大佬
+    Node pre, head;
+    public Node treeToDoublyList(Node root) {
+        // 边界值
+        if(root == null) return null;
+        dfs(root);
+
+        // 题目要求头尾连接
+        head.left = pre;
+        pre.right = head;
+        // 返回头节点
+        return head;
+    }
+    void dfs(Node cur) {
+        // 递归结束条件
+        if(cur == null) return;
+        dfs(cur.left);
+        // 如果pre为空，就说明是第一个节点，头结点，然后用head保存头结点，用于之后的返回
+        if (pre == null) head = cur;
+            // 如果不为空，那就说明是中间的节点。并且pre保存的是上一个节点，
+            // 让上一个节点的右指针指向当前节点
+        else if (pre != null) pre.right = cur;
+        // 再让当前节点的左指针指向父节点，也就连成了双向链表
+        cur.left = pre;
+        // 保存当前节点，用于下层递归创建
+        pre = cur;
+        dfs(cur.right);
+    }
+}
